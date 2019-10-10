c2c984ccab708a7f5b6d07e3aabca5249ea75d05
沉璧浮光 <120276969@qq.com>
2019-10-10 15:49:01 +0800

    add time receiver demo

---
+++ app/src/main/java/com/bcst/receiver/BcstReceiver.kt
@@ -0,0 +1,97 @@
+package com.bcst.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+/**
+ * 添加人：  Tom Hawk
+ * 添加时间：2019/10/10 14:42
+ * 功能描述：广播接收器封装
+ * <p>
+ * 修改人：  Tom Hawk
+ * 修改时间：2019/10/10 14:42
+ * 修改内容：
+ */
+class BcstReceiver : BroadcastReceiver() {
+    private val intentFilter: IntentFilter by lazy { IntentFilter() }
+    private var callback: ((context: Context, intent: Intent) -> Unit)? = null
+    private var hasRegisterReceiver = false
+
+    override fun onReceive(context: Context, intent: Intent) {
+        callback?.invoke(context, intent)
+    }
+
+    fun withFilter(filter: (IntentFilter) -> Unit): BcstReceiver {
+        filter.invoke(intentFilter)
+        return this
+    }
+
+    fun setCallback(callback: (context: Context, intent: Intent) -> Unit): BcstReceiver {
+        this.callback = callback
+        return this
+    }
+
+    fun register(context: Context) {
+        register(context, true)
+    }
+
+    fun unregister(context: Context) {
+        unregister(context, true)
+    }
+
+    /**
+     * for AppCompatActivity, Fragment, LifecycleService
+     */
+    fun bind(context: Context, lifecycle: Lifecycle, event: Lifecycle.Event = Lifecycle.Event.ON_CREATE) {
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                register(context, event == Lifecycle.Event.ON_CREATE)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            fun onStart() {
+                register(context, event == Lifecycle.Event.ON_START)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume() {
+                register(context, event == Lifecycle.Event.ON_RESUME)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause() {
+                unregister(context, event == Lifecycle.Event.ON_RESUME)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            fun onStop() {
+                unregister(context, event == Lifecycle.Event.ON_START)
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                unregister(context, event == Lifecycle.Event.ON_CREATE)
+            }
+        })
+    }
+
+    private fun register(context: Context, shouldRegister: Boolean) {
+        if (shouldRegister && !hasRegisterReceiver) {
+            context.registerReceiver(this, intentFilter)
+            hasRegisterReceiver = true
+        }
+    }
+
+    private fun unregister(context: Context, shouldUnregister: Boolean) {
+        if (shouldUnregister && hasRegisterReceiver) {
+            context.unregisterReceiver(this)
+            hasRegisterReceiver = false
+        }
+    }
+}
--- app/src/main/java/com/bcst/receiver/MainActivity.kt
+++ app/src/main/java/com/bcst/receiver/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.bcst.receiver
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTime.setOnClickListener(this)
+        btnBattery.setOnClickListener(this)
+        btnWifi.setOnClickListener(this)
+        btnHome.setOnClickListener(this)
+        btnScreen.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        v.isEnabled = false
+
+        when (v.id) {
+            R.id.btnTime -> {
+                BcstReceiver()
+                        .withFilter { intentFilter ->
+                            intentFilter.addAction(Intent.ACTION_TIME_CHANGED)
+                            intentFilter.addAction(Intent.ACTION_TIME_TICK)
+                        }
+                        .setCallback { _, _ -> Log.e("***", "${System.currentTimeMillis()}") }
+                        .bind(this, lifecycle)
+            }
+            R.id.btnBattery -> {
+            }
+            R.id.btnWifi -> {
+            }
+            R.id.btnHome -> {
+            }
+            R.id.btnScreen -> {
+            }
+        }
     }
 }
